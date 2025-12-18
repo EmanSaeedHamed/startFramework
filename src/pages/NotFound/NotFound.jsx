306f3f65e4e84f15ef6917a29d841a7f94fbb937
Eman Saeed <emansaeedhamed22220@gmail.com>
2025-12-18 08:20:13 +0200

    7commit

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,18 @@
+import './NotFound.css'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+      <Navbar />
+      <div className="not-found-container">
+        <div className="not-found-content text-center">
+          <h2>Not Found</h2>
+          <p>This page doesn’t exist.</p>
+        </div>
+      </div>
+      <Footer />
+    </>
   )
 }
